0925f724026d8a10901e7372be0efc8c1d41f760
Victhor137 <victhorfelix7@gmail.com>
2024-08-11 10:48:55 -0300

    Create 48 A 49 - Ler notas.py
    48) Escreva um algoritmo para ler as notas da 1a. e 2a. avaliações de um aluno, calcule e imprima a média (simples) desse aluno. Só devem ser aceitos valores válidos durante a leitura (0 a 10) para cada nota.
    
    49) Acrescente uma mensagem 'NOVO CÁLCULO (S/N)?' ao final do exercício [48]. Se for respondido 'S' deve retornar e executar um novo cálculo, caso contrário deverá encerrar o algoritmo.

---
+++ 48 A 49 - Ler notas.py
@@ -0,0 +1,17 @@
+# Condição
+resposta = "S"
+
+#bitree
+while resposta == "S":
+    nota1 =  float(input("Digite a nota da 1°Av: "))
+    nota2 =  float(input("Digite a nota da 2°Av: "))
+    #calc
+    media = (nota1 + nota2)/2    
+    print("A média final é {:.2f}".format(media))
+    #condição de retorno
+    reposta = str(input("Digite (S) para continuar e (N) para encerrar a sessão.")).upper()
+    if reposta == "S":
+        print("Reiniciando calculadora:")
+    else:
+        print("Fim da operação.")
+        break
