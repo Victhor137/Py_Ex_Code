def3a522eb4630ee258d363f9baa43405d1f9118
Victhor137 <victhorfelix7@gmail.com>
2024-08-16 19:34:42 -0300

    Atualização e correção

--- 61.py
+++ 61.py
@@ -2,11 +2,8 @@ x = []
 contador = 0
 while contador < 10:
     valor = int(input("Digite um valor: "))
-    if 9 < valor < 21:
-        x.append(valor)
-        contador = contador+1
-    else:
-        contador = contador+1
+    x.append(valor)
+    contador = contador+1
 print(x)
-calc = x[0] + x[1] + x[2] + x[3] + x[4] + x[5] + x[6] + x[7] + x[8] + x[9]
+calc = (x[0] + x[1] + x[2] + x[3] + x[4] + x[5] + x[6] + x[7] + x[8] + x[9]) / 10
 print(calc)
